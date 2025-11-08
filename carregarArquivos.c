b8157dd4599dab57e35d23e52481a9e09cf5eece
usermenob <senjumenob@gmail.com>
2025-11-08 18:23:56 -0300

    adição do sistema de upvotes

--- carregarArquivos.c
+++ carregarArquivos.c
@@ -10,7 +10,53 @@ struct Dados{
     char tipoAlerta;
 };
 
-int lerArquivo(struct Dados **registro){
+void criarUpvotes(char *nomeArquivoBrs, struct Dados dado){
+    FILE *original = fopen(nomeArquivoBrs, "r");
+    FILE *temporario = fopen("temporario.csv", "w");
+
+    if (temporario == NULL){
+        printf("Erro");
+        if (original) fclose(original);
+        return;        
+    }
+
+    int encontrado = 0;
+    char linha [256];
+    int idUsuario, timestamp, br, upvotes;
+    float km;
+    char tipoAlerta;
+
+    if (original != NULL){
+        while (fgets(linha, sizeof(linha), original)!=NULL){
+            if (sscanf(linha, "%d;%d;%d;%f;%c;%d", &idUsuario, &timestamp, &br, &km, &tipoAlerta, &upvotes)== 6){
+                if (km == dado.km && tipoAlerta == dado.tipoAlerta){
+                    upvotes++;
+                    encontrado = 1;
+                }
+                fprintf(temporario, "%d;%d;%d;%.2f;%c;%d\n", idUsuario, timestamp, br, km, tipoAlerta, upvotes);
+            } else if (sscanf(linha, "%d;%d;%d;%f;%c", &idUsuario, &timestamp, &br, &km, &tipoAlerta) == 5){
+                if (km == dado.km && tipoAlerta == dado.tipoAlerta){
+                    upvotes = 2;
+                    encontrado = 1;
+                } else {
+                    upvotes = 1;
+                }
+                fprintf(temporario, "%d;%d;%d;%.2f;%c;%d\n", idUsuario, timestamp, br, km, tipoAlerta, upvotes);
+            }
+        }
+        fclose(original);
+            /* code */
+        }
+        if (!encontrado){
+            fprintf(temporario, "%d;%d;%d;%.2f;%c;%d\n", dado.idUsuario, dado.timestamp, dado.br, dado.km, dado.tipoAlerta, 1);
+        }
+        fclose(temporario);
+
+        remove(nomeArquivoBrs);
+        rename("temporario.csv", nomeArquivoBrs);
+}
+
+int lerArquivo(){
     char nomeDoArquivo[100];
 
     printf("Digite o nome do arquivo que vai ser enviado:\n");
@@ -23,8 +69,6 @@ int lerArquivo(struct Dados **registro){
     }
     struct Dados temp;
     char linha[256];
-    int total = 0;
-    *registro = NULL;
 
         while (fgets(linha, sizeof(linha), arquivo)!= NULL){
             if(sscanf(linha,"%d;%d;%d;%f;%c", &temp.idUsuario, &temp.timestamp, &temp.br, &temp.km, &temp.tipoAlerta)==5){
@@ -33,28 +77,18 @@ int lerArquivo(struct Dados **registro){
             }else{
                 temp.km = (int)temp.km + 0.5;
             }
-                *registro = realloc(*registro, (total + 1)* sizeof(struct Dados));
-            (*registro)[total] = temp;
-            total++;
-            
+
             char nomeArquivoBrs[64];
             sprintf(nomeArquivoBrs, "%d.csv", temp.br);
-            FILE *saida = fopen(nomeArquivoBrs, "a");
-            if (saida == NULL){
-                printf("erro ao criar o arquivo %s\n", nomeArquivoBrs);
-                continue;
-            }
-            fprintf(saida, "%d;%d;%d;%.2f;%c\n", temp.idUsuario, temp.timestamp, temp.br, temp.km, temp.tipoAlerta);
-            fclose(saida);
+           
+            criarUpvotes(nomeArquivoBrs, temp);
         }
     }
     fclose(arquivo);
-    return total;
+    return 0;
 }
 
 int main(){
-    struct Dados *registro = NULL;
-    int total = lerArquivo(&registro);
-    free(registro);
+    lerArquivo();
     return 0;
 }
